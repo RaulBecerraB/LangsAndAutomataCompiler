9562d1117dcc9195190ee1a4476183f46fffa847
RaulBecerraB <ingenierobecerrabarcelo@gmail.com>
2025-02-18 02:12:59 -0600

    feat(homepage): add homepage with information
        - Implemented `Homepage.jsx` with introductory content.
        - Added layout and styling for a clean presentation.
        - Ensured responsiveness and accessibility.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,8 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className="container">
+      <h1>Compilador Unidad 1 - Lenguajes y autómatas II</h1>
+      <h1>Equipo #5</h1>
+      <h2 className="mt-10">Becerra Barceló José Raúl. E21080774</h2>
+      <h2>Diaz Sanches Edwin Ulises. E21080774</h2>
+      <h2>Ferrer Chacón Diego Felipe. E21080738</h2>
+      <h2 className="mt-10">7SA</h2>
     </div>
   );
 }
